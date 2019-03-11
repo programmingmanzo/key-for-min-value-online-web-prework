cfd8cebc26c372f937b769791acb9c0c4b75f9aa
Gerardo Manzo <programmingmanzo@gmail.com>
2019-03-11 01:38:01 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,31 +1,32 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
-   if name_hash[0] = nil 
-     return nil 
-   else
-  #if
-    min_key = name_hash.first[0]
-    min_value = name_hash.first[1]
-    name_hash.each do |k, v|
-      if v < min_value 
-        min_value = v
-        min_key = k
-      end
-    end
-    min_key
-  end
-end
-
+#Will work on this code to make it work later...
 # def key_for_min_value(name_hash)
-#   min_key = nil 
-#   min_value = nil 
-#   name_hash.collect do |k, v|
-#     if min_value == nil || v < min_value
-#       min_value = v 
-#       min_key = k 
-#     end 
-#   end 
-#   min_key
-# end 
+#   if name_hash[0] = nil 
+#     return nil 
+#   else
+#   #if
+#     min_key = name_hash.first[0]
+#     min_value = name_hash.first[1]
+#     name_hash.each do |k, v|
+#       if v < min_value 
+#         min_value = v
+#         min_key = k
+#       end
+#     end
+#     min_key
+#   end
+# end
+
+def key_for_min_value(name_hash)
+  min_key = nil 
+  min_value = nil 
+  name_hash.collect do |k, v|
+    if min_value == nil || v < min_value
+      min_value = v 
+      min_key = k 
+    end 
+  end 
+  min_key
+end 
